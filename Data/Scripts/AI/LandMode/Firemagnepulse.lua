9e0d02e51c4ab6296e063d74698d20edda829d45
Alexander Sudnikovich <asudno@gmail.com>
2020-11-03 22:53:21 +0300

    Revert "Revert "Merge branch 'dev' of https://github.com/Xnuman/New-Order-Mod into dev""
    This reverts commit 632386b68cefd4ca3608249043f122c5c594c693.

---
+++ Data/Scripts/AI/LandMode/Firemagnepulse.lua
@@ -0,0 +1,61 @@
+-- $Id: //depot/Projects/StarWars_Expansion/Run/Data/Scripts/AI/LandMode/FireMagnepulse.lua#1 $
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+--
+-- (C) Petroglyph Games, Inc.
+--
+--
+--  *****           **                          *                   *
+--  *   **          *                           *                   *
+--  *    *          *                           *                   *
+--  *    *          *     *                 *   *          *        *
+--  *   *     *** ******  * **  ****      ***   * *      * *****    * ***
+--  *  **    *  *   *     **   *   **   **  *   *  *    * **   **   **   *
+--  ***     *****   *     *   *     *  *    *   *  *   **  *    *   *    *
+--  *       *       *     *   *     *  *    *   *   *  *   *    *   *    *
+--  *       *       *     *   *     *  *    *   *   * **   *   *    *    *
+--  *       **       *    *   **   *   **   *   *    **    *  *     *   *
+-- **        ****     **  *    ****     *****   *    **    ***      *   *
+--                                          *        *     *
+--                                          *        *     *
+--                                          *       *      *
+--                                      *  *        *      *
+--                                      ****       *       *
+--
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+-- C O N F I D E N T I A L   S O U R C E   C O D E -- D O   N O T   D I S T R I B U T E
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+--
+--              $File: //depot/Projects/StarWars_Expansion/Run/Data/Scripts/AI/LandMode/FireMagnepulse.lua $
+--
+--    Original Author: Steve_Copeland
+--
+--            $Author: Andre_Arsenault $
+--
+--            $Change: 37816 $
+--
+--          $DateTime: 2006/02/15 15:33:33 $
+--
+--          $Revision: #1 $
+--
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+
+-- Self-attachment script for the Ion Canno, but currently a custom goal 
+-- is the best tactic 
+
+require("pgevents")
+
+
+function Definitions()	
+	Category = "AlwaysOff"
+	TaskForce = {
+	{
+		"ReserveForce"
+		,"DenyHeroAttach"
+		,"TaskForceRequired"
+	}
+	}
+end
+
+function ReserveForce_Thread()		
+	ScriptExit()
+end
---
+++ Data/Scripts/AI/LandMode/Palpatinemanager.lua
@@ -0,0 +1,61 @@
+-- $Id: //depot/Projects/StarWars_Expansion/Run/Data/Scripts/AI/LandMode/PalpatineManager.lua#4 $
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+--
+-- (C) Petroglyph Games, Inc.
+--
+--
+--  *****           **                          *                   *
+--  *   **          *                           *                   *
+--  *    *          *                           *                   *
+--  *    *          *     *                 *   *          *        *
+--  *   *     *** ******  * **  ****      ***   * *      * *****    * ***
+--  *  **    *  *   *     **   *   **   **  *   *  *    * **   **   **   *
+--  ***     *****   *     *   *     *  *    *   *  *   **  *    *   *    *
+--  *       *       *     *   *     *  *    *   *   *  *   *    *   *    *
+--  *       *       *     *   *     *  *    *   *   * **   *   *    *    *
+--  *       **       *    *   **   *   **   *   *    **    *  *     *   *
+-- **        ****     **  *    ****     *****   *    **    ***      *   *
+--                                          *        *     *
+--                                          *        *     *
+--                                          *       *      *
+--                                      *  *        *      *
+--                                      ****       *       *
+--
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+-- C O N F I D E N T I A L   S O U R C E   C O D E -- D O   N O T   D I S T R I B U T E
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+--
+--              $File: //depot/Projects/StarWars_Expansion/Run/Data/Scripts/AI/LandMode/PalpatineManager.lua $
+--
+--    Original Author: Steve_Copeland
+--
+--            $Author: James_Yarrow $
+--
+--            $Change: 52287 $
+--
+--          $DateTime: 2006/08/22 10:41:09 $
+--
+--          $Revision: #4 $
+--
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+
+-- This plan simply puts palpatine in the right places.
+-- It relies on the object script darthpalpatine.lua to activate abilities.
+
+require("pgevents")
+
+
+function Definitions()	
+	Category = "AlwaysOff"
+	TaskForce = {
+	{
+		"ReserveForce"
+		,"DenyHeroAttach"
+		,"TaskForceRequired"
+	}
+	}
+end
+
+function ReserveForce_Thread()		
+	ScriptExit()
+end
