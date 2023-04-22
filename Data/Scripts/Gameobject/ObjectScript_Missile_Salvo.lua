fe9c39f0ea095f6d9610f7e0821e38cada80af4d
Alexander Sudnikovich <sudnikovichAL@mail.ru>
2023-04-22 10:51:22 +0300

    [LUA] Interdictor and Missle Salvo objectscripts from dev_space

---
+++ Data/Scripts/Gameobject/ObjectScript_Missile_Salvo.lua
@@ -0,0 +1,113 @@
+-- $Id: //depot/Projects/StarWars_Expansion/Run/Data/Scripts/GameObject/ObjectScript_Krayt.lua#6 $
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+--
+-- (C) Petroglyph Games, Inc.
+--
+--
+--  *****           **                          *                   *
+--  *   **          *                           *                   *
+--  *    *          *                           *                   *
+--  *    *          *     *                 *   *          *        *
+--  *   *     *** ******  * **  ****      ***   * *      * *****    * ***
+--  *  **    *  *   *     **   *   **   **  *   *  *    * **   **   **   *
+--  ***     *****   *     *   *     *  *    *   *  *   **  *    *   *    *
+--  *       *       *     *   *     *  *    *   *   *  *   *    *   *    *
+--  *       *       *     *   *     *  *    *   *   * **   *   *    *    *
+--  *       **       *    *   **   *   **   *   *    **    *  *     *   *
+-- **        ****     **  *    ****     *****   *    **    ***      *   *
+--                                          *        *     *
+--                                          *        *     *
+--                                          *       *      *
+--                                      *  *        *      *
+--                                      ****       *       *
+--
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+-- C O N F I D E N T I A L   S O U R C E   C O D E -- D O   N O T   D I S T R I B U T E
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+--
+--              $File: //depot/Projects/StarWars_Expansion/Run/Data/Scripts/GameObject/ObjectScript_Krayt.lua $
+--
+--    Original Author: James Yarrow
+--
+--            $Author: James_Yarrow $
+--
+--            $Change: 49249 $
+--
+--          $DateTime: 2006/07/20 13:58:07 $
+--
+--          $Revision: #6 $
+--
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+
+require("PGStateMachine")
+
+function Definitions()
+	ServiceRate = 1
+
+	Define_State("State_Init", State_Init);
+	Define_State("State_AI_Autofire", State_AI_Autofire)
+	Define_State("State_Human_No_Autofire", State_Human_No_Autofire)
+	Define_State("State_Human_Autofire", State_Human_Autofire)
+	
+	ability_name = "BLAST"
+end
+
+function State_Init(message)
+	if message == OnEnter then
+
+		-- prevent this from doing anything in galactic mode
+		if Get_Game_Mode() ~= "Space" then
+			ScriptExit()
+		end
+		
+		if Object.Get_Owner().Is_Human() then
+			Set_Next_State("State_Human_No_Autofire")
+		else
+			Set_Next_State("State_AI_Autofire")
+		end
+	end
+end
+
+function State_Human_Autofire(message)
+	if message == OnUpdate then
+		if Object.Is_Ability_Autofire(ability_name) then
+			Try_Missile_Salvo()
+		else
+			Set_Next_State("State_Human_No_Autofire")
+		end
+	end				
+end
+
+function State_AI_Autofire(message)
+	if message == OnUpdate then
+		Try_Missile_Salvo()
+	end		
+end
+
+function State_Human_No_Autofire(message)
+	if message == OnUpdate then
+		if Object.Is_Ability_Autofire(ability_name) then
+			Set_Next_State("State_Human_Autofire")
+		end
+	end
+end
+
+function Try_Missile_Salvo()
+	if Object.Is_Ability_Ready(ability_name) then
+		DebugMessage("Ability ready")
+		if Object.Has_Attack_Target() then
+			DebugMessage("Has_Attack_Target")
+			target = Object.Get_Attack_Target()
+			DebugMessage("Attacking %s %s %s", tostring(target), tostring(target.Get_Hull()), tostring(target.Get_Shield()))
+			if (target.Is_Category("Structure") or target.Is_Category("Frigate") or target.Is_Category("Capital") or target.Is_Category("Super")) and target.Get_Shield() < 0.1 then
+				Try_Ability(Object, ability_name)
+				if TestValid(target) then
+					Object.Attack_Target(target)
+					DebugMessage("Done!")
+				else
+					DebugMessage("Not valid target!")
+				end
+			end
+		end
+	end
+end
